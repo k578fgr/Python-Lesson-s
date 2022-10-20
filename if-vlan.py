ab4797f0b2d4d3c0c7c6ec3c6f6709749cb06a60
k578fgr <gnatko.kirill@mail.ru>
2022-10-21 00:09:26 +0400

    add if-vlan file

---
+++ if-vlan.py
@@ -0,0 +1,7 @@
+nativeVlan = 1
+dataVlan = 100
+
+if nativeVlan == dataVlan:
+    print("The native VLAN and the data VLAN are the same. ")
+else:
+    print("The native VLAN and the data VLAN are diffrent" )
