c22f5bfd46d2be4e8883d607569285c7896ef943
k578fgr <gnatko.kirill@mail.ru>
2022-10-23 02:05:40 +0400

    added devices.append

--- file-access.py
+++ file-access.py
@@ -1,8 +1,10 @@
 #Режим w -write, r - read, a - append(дописать)
+devices = []
 file = open("devices.txt", "r")
 
 for item in file:
     item = item.strip()
     print(item)
-
+    devices.append(item)
 file.close()
+print(devices)
